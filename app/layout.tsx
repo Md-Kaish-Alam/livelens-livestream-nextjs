6cd59c9a06ef8f997ec8223b521da9b41dccb4da
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-12-19 23:34:43 +0530

    style: implement dark mode in applicatio and also in clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 import "./globals.css";
 
@@ -13,11 +16,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon.png',
-        href: '/icon.png',
-      }
-    ]
-  }
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -26,9 +29,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="dark"
+            storageKey="live-lens"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home () {
   return (
     <div>
       <UserButton afterSignOutUrl="/"/>
+      <Image src='/logo.svg' width={500} height={500} alt="logo" />
     </div>
   )
 }
