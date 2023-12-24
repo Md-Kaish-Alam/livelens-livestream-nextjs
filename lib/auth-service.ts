6d8f5892bb2ba51999c3c1049c1564e505b39c35
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-12-25 05:07:05 +0530

    build: create the streamer dashboard page with proper handling of errors and loading states

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -21,3 +21,28 @@ export const getSelf = async () => {
 
   return user;
 };
+
+
+export const getSelfByUsername = async (username: string) =>{
+  const self = await currentUser();
+
+  if(!self || !self.username) {
+    throw new Error("Unauthorized User");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username: self.username,
+    }
+  })
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if(self.username !== user.username) {
+    throw new Error("Unauthenticated User");
+  }
+
+  return user;
+}
