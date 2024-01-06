6fcb9ee30783b11c6e99f8f2737f04fb9492925c
Md Kaish Alam <alamkaishg1511@gmail.com>
2024-01-06 07:36:38 +0530

    build: completed the home page desing with the stream cards

---
+++ components/thumbnail.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
+import { UserAvatar } from "@/components/user-avatar";
+
+interface ThumbnailProps {
+  src: string | null;
+  fallback: string;
+  isLive: boolean;
+  username: string;
+}
+
+export const Thumbnail = ({
+  src,
+  fallback,
+  isLive,
+  username,
+}: ThumbnailProps) => {
+  let content;
+
+  if (!src) {
+    content = (
+      <div className="bg-background flex flex-col items-center justify-center gap-y-4 w-full h-full transition-transform group-hover:translate-x-2 group-hover:-translate-y-2 rounded-md">
+        <UserAvatar
+          size="lg"
+          showBadge
+          username={username}
+          imageUrl={fallback}
+          isLive={isLive}
+        />
+      </div>
+    );
+  } else {
+    content = (
+      <Image
+        src={src}
+        alt="username"
+        fill
+        className="object-cover transition-transform group-hover:translate-x-2 group-hover:-translate-y-2 rounded-md"
+      />
+    );
+  }
+  return (
+    <div className="group aspect-video relative rounded-md cursor-pointer">
+      <div
+        className={cn(
+          "rounded-md absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center",
+          isLive ? "bg-rose-600" : "bg-blue-600"
+        )}
+      />
+      {content}
+    </div>
+  );
+};
+
+export const ThumbnailSkeleton = () => {
+  return (
+    <div className="group aspect-video relative rounded-xl cursor-pointer">
+      <Skeleton className="h-full w-full" />
+    </div>
+  )
+}
