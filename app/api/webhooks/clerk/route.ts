81781f41eb7054c9c217c269e4975955d19c71bd
Md-Kaish-Alam <alamkaishg1511@gmail.com>
2025-03-14 06:53:44 +0530

    fix: Clerk middleware, Clerk webhook and prisma orm issues

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -15,15 +15,18 @@ export async function POST(req: Request) {
     );
   }
 
-  // Get the headers
-  const headerPayload = headers();
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  // Get headers
+  const headerPayload = await headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response("Error: Missing Svix headers", {
       status: 400,
     });
   }
@@ -32,12 +35,9 @@ export async function POST(req: Request) {
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
+  // Verify payload with headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -45,19 +45,15 @@ export async function POST(req: Request) {
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
       status: 400,
     });
   }
 
+
   // Get the ID and type
   const eventType = evt.type;
-
-  console.log({
-    called: true,
-    payload: payload,
-  });
 
   if (eventType === "user.created") {
     await db.user.create({
@@ -95,7 +91,7 @@ export async function POST(req: Request) {
     });
   }
 
-  return new Response("", { status: 200 });
+  return new Response("Webhook received", { status: 200 });
 }
 
 export async function GET() {
